d5b91e37887e23764874873d0e89cbfbc18ab14f
Mateusz laptop <malicki9mateusz4@gmail.com>
2021-11-29 22:01:44 +0100

    Ostateczne modyfikacje

--- HANGMAN_PL_MP.py
+++ HANGMAN_PL_MP.py
@@ -2,69 +2,70 @@ import re #biblioteka zawierająca między innymi funkcję do znalezienia indesk
 import HANGMAN_PENALTY
 import getpass  #<- do sprawdzenia
 
-#word = "warszawa"  <- do testowania
-#word = (input("Jakie słowo będzie zgadywane? ")) #jakies tam slowo do testu
+def HANGMAN_MP_GAME():
+    #word = "warszawa"  <- do testowania
+    #word = (input("Jakie słowo będzie zgadywane? ")) #jakies tam slowo do testu
 
-word = getpass.getpass(prompt = "Jakie słowo będzie zgadywane?").casefold() #<- działa, ale nie w pycharmie
-word_length = len(word)
+    word = getpass.getpass(prompt = "Jakie słowo będzie zgadywane?").casefold() #<- działa, ale nie w pycharmie
+    word_length = len(word)
 
-word_hidden = []
+    word_hidden = []
 
-for i in range(0,word_length): #tworzenie listy ukazującej szukane słowo jako ciąg _
-    word_hidden.append("_")
-print(f"Szukane słowo składa się z {len(word_hidden)} liter:")
-print(' '.join(word_hidden)) #zamiana listy na string, separatorem między elementami listy jest spacja
+    for i in range(0,word_length): #tworzenie listy ukazującej szukane słowo jako ciąg _
+        word_hidden.append("_")
+    print(f"Szukane słowo składa się z {len(word_hidden)} liter:")
+    print(' '.join(word_hidden)) #zamiana listy na string, separatorem między elementami listy jest spacja
 
-penalty = 0 #zadanie zmiennej penalty
-input_history = [] #otwarcie listy z inputami (co by sie nie powtarzać)
+    penalty = 0 #zadanie zmiennej penalty
+    input_history = [] #otwarcie listy z inputami (co by sie nie powtarzać)
 
-while penalty < 12:
-    print("")
-    taken_symbol = 1
-    while taken_symbol == 1: #pętla do sprawdzenia, czy wprowadzana litera, lub słowo się powtórzyło
-        symbol = input("Wybierz literę, lub odgadnij całe słowo: ").casefold()  # wybieranie literki przez gracza
+    while penalty < 12:
         print("")
-        if input_history.count(symbol) > 0:
-            taken_symbol = 1
-            print("Litera , lub słowo zostało już wybrane!")
+        taken_symbol = 1
+        while taken_symbol == 1: #pętla do sprawdzenia, czy wprowadzana litera, lub słowo się powtórzyło
+            symbol = input("Wybierz literę, lub odgadnij całe słowo: ").casefold()  # wybieranie literki przez gracza
+            print("")
+            if input_history.count(symbol) > 0:
+                taken_symbol = 1
+                print("Litera , lub słowo zostało już wybrane!")
 
-        else:
-            taken_symbol = 0
-            input_history.append(symbol)
+            else:
+                taken_symbol = 0
+                input_history.append(symbol)
 
-    symbol_index = [_.start() for _ in re.finditer(symbol, word)]  # znalezienie WSZYSTKICH znaków w stringu
-    #print(symbol_index)
-    #print(len(symbol_index))
+        symbol_index = [_.start() for _ in re.finditer(symbol, word)]  # znalezienie WSZYSTKICH znaków w stringu
+        #print(symbol_index)
+        #print(len(symbol_index))
 
-    if symbol == word: #sprawdzenie czy odgadnięto słowo
-        print(f"Tak, to {word}, WYGRAŁEŚ/ WYGRAŁAŚ!!!")
-        break
-    elif symbol != word: #przypadek gdy słowno nie zostało do razu odgadnięte
-        if len(symbol) == 1: #sprawdzenie czy wybrana literka się zgadza
+        if symbol == word: #sprawdzenie czy odgadnięto słowo
+            print(f"Tak, to {word}, WYGRAŁEŚ/ WYGRAŁAŚ!!!")
+            break
+        elif symbol != word: #przypadek gdy słowno nie zostało do razu odgadnięte
+            if len(symbol) == 1: #sprawdzenie czy wybrana literka się zgadza
 
-            if len(symbol_index) == 0: #jeżeli się nie zgadza
+                if len(symbol_index) == 0: #jeżeli się nie zgadza
+                    penalty += 1
+                    HANGMAN_PENALTY.hangman_penalty(penalty)
+                    print(' '.join(word_hidden))
+
+                else: #jeżeli się zgadza
+                    inserted_value = ""
+                    for i in range(0, len(symbol_index)):  # wstawianie wybranej literki do listy
+                        inserted_value = symbol_index[i]
+                        word_hidden[inserted_value] = word[inserted_value]
+                    print(' '.join(word_hidden))
+            else: #jeżeli wpisano ciągn znaków nie zgadzający się z odgadywanym słowem
                 penalty += 1
                 HANGMAN_PENALTY.hangman_penalty(penalty)
                 print(' '.join(word_hidden))
 
-            else: #jeżeli się zgadza
-                inserted_value = ""
-                for i in range(0, len(symbol_index)):  # wstawianie wybranej literki do listy
-                    inserted_value = symbol_index[i]
-                    word_hidden[inserted_value] = word[inserted_value]
-                print(' '.join(word_hidden))
-        else: #jeżeli wpisano ciągn znaków nie zgadzający się z odgadywanym słowem
-            penalty += 1
-            HANGMAN_PENALTY.hangman_penalty(penalty)
-            print(' '.join(word_hidden))
 
+        if (''.join(word_hidden)) == word:
+            print(f"Tak, to {word}, WYGRAŁEŚ/ WYGRAŁAŚ!!!")
+            break
 
-    if (''.join(word_hidden)) == word:
-        print(f"Tak, to {word}, WYGRAŁEŚ/ WYGRAŁAŚ!!!")
-        break
-
-    if penalty == 12:
-        print(f"Szukanym słowem było: {word}")
+        if penalty == 12:
+            print(f"Szukanym słowem było: {word}")
 
 
 
--- THE_GAME.py
+++ THE_GAME.py
@@ -1,5 +1,8 @@
 from PAPER_VS_ROCK_VS_SCISSORS_PL_SP import PRS_Single_game
 from PAPER_VS_ROCK_VS_SCISSORS_PL_MP import PRS_Multi_game
+from CIRCLE_AND_CROSS_PL import CAC_game
+from HANGMAN_PL_MP import HANGMAN_MP_GAME
+from HANGMAN_PL_SP import HANGMAN_SP_GAME
 
 print("Witamy w aplikacji THE GAME")
 print("")
@@ -20,9 +23,17 @@ while game_on == "t":
     if game == "1": #kółko i krzyżyk
         game_on_1 = "t"
         while game_on_1 == "t":
-            import CIRCLE_AND_CROSS_PL
+            CAC_game()
 
             game_on_1 = input("Gramy dalej w kółko i krzyżyk?(t-tak/n-nie): ").casefold()
+            while True:
+
+                if game_on_1 == "t" or game_on_1 == "n":
+                    break
+                else:
+                    print("Zły znak!!")
+                    print("")
+                    game_on_1 = input("Gramy dalej w kółko i krzyżyk?(t-tak/n-nie): ").casefold()
             print("")
 
     elif game == "2": #kamień papier noożyce
@@ -30,7 +41,6 @@ while game_on == "t":
         while game_on_2 == "t":
             prs_choice =""
             prs_choice = input("Wybierz tryb gry: pojedynczy gracz(s)/ dwóch graczy(m): ").casefold()
-
             while True:
 
                 if prs_choice == "s" or prs_choice == "m":
@@ -52,27 +62,58 @@ while game_on == "t":
             print("")
 
             game_on_2 = input("Gramy dalej w kamień, papier, nożyce?(t-tak/n-nie): ").casefold()
+            while True:
+
+                if game_on_2 == "t" or game_on_2 == "n":
+                    break
+                else:
+                    print("Zły znak!!")
+                    print("")
+                    game_on_2 = input("Gramy dalej w kamień, papier, nożyce?(t-tak/n-nie): ").casefold()
             print("")
 
     elif game == "3":
         game_on_3 = "t"
         while game_on_3 == "t":
-            hang_choice =""
+            hang_choice: str =""
             hang_choice = input("Wybierz tryb gry: pojedynczy gracz(s)/ dwóch graczy(m): ").casefold()
-            print(hang_choice)
+            while True:
+
+                if hang_choice == "s" or hang_choice == "m":
+                    break
+                else:
+                    print("Zły znak!!")
+                    print("")
+                    hang_choice = input("Wybierz tryb gry: pojedynczy gracz(s)/ dwóch graczy(m): ").casefold()
+
             if hang_choice == "s":
-                #from HANGMAN import HANGMAN_PL_SP
-                import HANGMAN_PL_SP
+                HANGMAN_SP_GAME()
 
             elif hang_choice == "m":
-                #from HANGMAN import HANGMAN_PL_MP
-                import HANGMAN_PL_MP
+                HANGMAN_MP_GAME()
+
             print("")
 
             game_on_3 = input("Gramy dalej w wisielca?(t-tak/n-nie): ").casefold()
+            while True:
+
+                if game_on_3 == "t" or game_on_3 == "n":
+                    break
+                else:
+                    print("Zły znak!!")
+                    print("")
+                    game_on_3 = input("Gramy dalej w wisielca?(t-tak/n-nie): ").casefold()
             print("")
 
     game_on = input("Chcesz wybrać nową grę?(t-tak/n-nie): ").casefold()
+    while True:
+
+        if game_on == "t" or game_on == "n":
+            break
+        else:
+            print("Zły znak!!")
+            print("")
+            game_on = input("Chcesz wybrać nową grę?(t-tak/n-nie): ").casefold()
     print("")
 
 print("")
